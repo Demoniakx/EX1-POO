721e2a886a27351d60a167dd42ee67cbd869106a
Thomas DC <148505996+Demoniakx@users.noreply.github.com>
2024-11-30 16:31:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,10 @@ import { CompteBancaire } from "./CompteBancaire.js";
 
 const compte = new CompteBancaire(1000);
 
-// Dépôt
 compte.ajoutDepot(200); 
 
-// Retrait
 compte.retraitSolde(150);  
 
-// Intérêts
 compte.soldeInteret(0.03); 
 
-// Afficher l'historique des opérations
 compte.afficherHistorique();
